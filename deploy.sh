48c68f57e30812d8bcf4efb56a21893bc00588d2
Amatya Joshi <joshiamatya@gmail.com>
2025-07-18 04:08:49 +0530

    Retail Edge

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 set -e
 
-# Cleanup
-rm -rf node_modules
-rm -rf client/node_modules
-rm -rf server/node_modules
-rm -f node_modules.tar.gz
-rm -f client/node_modules.tar.gz
-rm -f server/node_modules.tar.gz
-
-# Build frontend
-cd client
-rm -rf node_modules
-npm install
-npm run build
-npm run export
-cd ..
-
-# Copy static build to server (for static export)
-rm -rf server/out
-cp -r client/out server/out
-
-# Build backend
+# Start the backend server (assumes everything is already built and installed)
 cd server
-rm -rf node_modules
-npm install
-npm run build 
+npm start 
